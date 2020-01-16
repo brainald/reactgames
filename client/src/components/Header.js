15327a84a9e10899b512dfc77cb46b222d15c2f6
Rainald Balog <rainald.balog@arobs.com>
2020-01-16 06:40:45 -0800

    test..

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -13,7 +13,7 @@ const Header = props => {
       <h1>
         <Link to="/">{props.text}</Link>
       </h1>
-      <a href="_" onClick={event => logOut(event)}>
+      <a href="_logout" onClick={event => logOut(event)}>
         Logout
       </a>
     </header>
